5b1e180e66687fa26571aa156e6914d26be4b60f
purnasri053 <purnaofficial.053@gmail.com>
2026-04-01 13:54:17 +0530

    fix: lazy init so app starts before DB connection

--- backend-spring/src/main/java/com/cvscanner/CvScannerApplication.java
+++ backend-spring/src/main/java/com/cvscanner/CvScannerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CvScannerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CvScannerApplication.class, args);
+        SpringApplication app = new SpringApplication(CvScannerApplication.class);
+        app.setLazyInitialization(true);
+        app.run(args);
     }
 }
